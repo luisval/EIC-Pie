ccd7110d8e85acea4faf6be6d3ee2094111f1c4c
luisval <luisvc3737@gmail.com>
2021-07-16 15:09:47 -0500

    Add files via upload

--- CalData/macro/Pieefficiency.C
+++ CalData/macro/Pieefficiency.C
@@ -14,84 +14,69 @@
 Int_t Pieefficiency()
 {
 
-
 TFile *file1 = TFile::Open("G4EICDetector_out_e.root");
 TFile *file2 = TFile::Open("G4EICDetector_out_pi.root");
 
-   if (file1->IsZombie()) {
-      cout << "Error opening file 1" << endl;
-      exit(-1);
-      } else {
-            cout << "File 1 is okay" << endl;
-   }
-
-  gStyle->SetOptTitle(0); //No title for histograms
-  gStyle->SetOptStat(0);
-
- TCanvas *c1 = new TCanvas("c1","EMCal");
-
+   if (file1 && file2->IsZombie()) { cout << "Error opening a file" << endl;
+       exit(-1);} 
+   else {cout << "Files are okay" << endl;}
+ 
+//  gStyle->SetOptTitle(0); //No title for histograms
+ // gStyle->SetOptStat(0);
+  
   TH1F *h1_EMCal_Ep = (TH1F*)file1->Get("h_EMCal_Ep");
- // h_EMCal_Ep->GetXaxis()->SetRangeUser(0,3);
- // h_EMCal_Ep->SetStats(kFALSE);
-  h1_EMCal_Ep->SetMarkerStyle(20);
+        h1_EMCal_Ep->GetXaxis()->SetTitle("EMCal E/p");
+        h1_EMCal_Ep->GetYaxis()->SetTitle("Counts");
 
-   h1_EMCal_Ep->SetFillColorAlpha(40, 0.1);
-   h1_EMCal_Ep->SetLineColorAlpha(kBlack, 0.35);
-//h1_EMCal_Ep->SetFillColor(kViolet); 
+  TH1F *h2_EMCal_Ep = (TH1F*)file2->Get("h_EMCal_Ep");
+        h2_EMCal_Ep->GetXaxis()->SetTitle("EMCal E/p");
+        h2_EMCal_Ep->GetYaxis()->SetTitle("Counts");
 
+        Int_t nbinsh1 = h1_EMCal_Ep->Integral();
+        Int_t nbinsh2 = h2_EMCal_Ep->Integral();
+        Int_t nbintstotal = nbinsh1+nbinsh2;
 
-  h1_EMCal_Ep->GetXaxis()->SetTitle("EMCal E/p");
-  h1_EMCal_Ep->GetYaxis()->SetTitle("Counts");
-  //c1->SetLogy();
-  h1_EMCal_Ep->SetDirectory(0);
-  file1->Close();
- // h_EMCal_Ep->Scale(1./h_EMCal_Ep->Integral());
-  h1_EMCal_Ep->Draw("same");
+        cout <<"nbinsh1: " << nbinsh1 << endl;
+        cout <<"nbinsh2: " << nbinsh2 << endl;
+        cout <<"Total bins: " << nbintstotal << endl;
 
-    TH1F *h2_EMCal_Ep = (TH1F*)file2->Get("h_EMCal_Ep");
-      h2_EMCal_Ep->SetMarkerStyle(21);
+        h1_EMCal_Ep->Scale(1./(nbintstotal));
+        h2_EMCal_Ep->Scale(1./(nbintstotal));
 
+           //h1_EMCal_Ep->Scale(1./(h1_EMCal_Ep)->Integral());
+           //h2_EMCal_Ep->Scale(1./(h2_EMCal_Ep)->Integral());
 
-   h2_EMCal_Ep->SetFillColorAlpha(41, 0.1);
-     h2_EMCal_Ep->SetLineColorAlpha(kBlue, 0.35);
-   // h2_EMCal_Ep->SetFillColor(kBlue); 
-   // h2_EMCal_Ep->SetFillColor(1);
-   // h2_EMCal_Ep->SetFillStyle(3001);
-
-  h2_EMCal_Ep->GetXaxis()->SetTitle("EMCal E/p");
-  h2_EMCal_Ep->GetYaxis()->SetTitle("Counts");
-  //c1->SetLogy();
-  h2_EMCal_Ep->SetDirectory(0);
-  file1->Close();
- // h_EMCal_Ep->Scale(1./h_EMCal_Ep->Integral());
-  h2_EMCal_Ep->Draw("same");
+          h1_EMCal_Ep->SetMarkerStyle(20);
+          h2_EMCal_Ep->SetMarkerStyle(24);
 
 
+  TCanvas *c1 = new TCanvas("c12","EMCal E/p");
+//  gStyle->SetOptStat(false);
+  c1->SetRightMargin(0.0465116);
+  c1->SetTopMargin(0.1);
+  c1->SetFillColor(0);
+  
+        h1_EMCal_Ep->Draw("same");
+        h2_EMCal_Ep->Draw("same");
 
-   TLegend *leg = new TLegend(0.65,0.8,0.92,0.89);
-   leg->SetTextFont(62);                                
-   leg->SetLineColor(0);
-   leg->SetLineStyle(0);
-   leg->SetLineWidth(1);
-   leg->SetFillColor(0);
-   leg->SetFillStyle(1001);
-   leg->AddEntry("","Particles","");
-   leg->AddEntry("h1_EMCal_Ep","e^{-}","f");
-   leg->AddEntry("h2_EMCal_Ep","#pi^{-}","f");
-   leg->Draw();
-
-     c1->SaveAs("Ep_pie.pdf");
-
-
-
-///////////////////////////////////////////////////////////////////////////////////////////
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+  TLegend *leg = new TLegend(0.7,0.6,0.92,0.89);
+  leg->SetTextFont(62);
+  leg->SetLineColor(0);
+  leg->SetLineStyle(0);
+  leg->SetLineWidth(1);
+  leg->SetFillColor(0);
+  leg->SetFillStyle(1001);
+  leg->AddEntry("","Particle","");
+  leg->AddEntry("h1_EMCal_Ep","e^{-}","p");
+  leg->AddEntry("h2_EMCal_Ep","#pi^{-}","p");
+  leg->Draw();
+  c1->SaveAs("Ep_pie.pdf");
+  
+ ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
  // file1->Close();
+ // file2->Close();
 
   cout<<"End of the program. Gracias"<<endl;
-  return 0;
-   
+  return 0;  
 }
 
