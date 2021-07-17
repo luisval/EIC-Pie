bd837995b2bc2f4c8b9a7a6d019c4af9e0065cbf
luisval <luisvc3737@gmail.com>
2021-07-16 20:15:49 -0500

    Add files via upload

--- CalData/macro/Pieefficiency.C
+++ CalData/macro/Pieefficiency.C
@@ -27,17 +27,19 @@ TFile *file2 = TFile::Open("G4EICDetector_out_pi.root");
   TH1F *h1_EMCal_Ep = (TH1F*)file1->Get("h_EMCal_Ep");
         h1_EMCal_Ep->GetXaxis()->SetTitle("EMCal E/p");
         h1_EMCal_Ep->GetYaxis()->SetTitle("Counts");
-        h1_EMCal_Ep->SetMarkerStyle(kFullTriangleUp);
+        h1_EMCal_Ep->SetFillColor(41); 
+      //  h1_EMCal_Ep->SetMarkerStyle(kFullTriangleUp);
        // h1_EMCal_Ep->SetMarkerStyle(20);
-       h1_EMCal_Ep->SetMarkerColor(1);
+      // h1_EMCal_Ep->SetMarkerColor(1);
 
 
   TH1F *h2_EMCal_Ep = (TH1F*)file2->Get("h_EMCal_Ep");
         h2_EMCal_Ep->GetXaxis()->SetTitle("EMCal E/p");
         h2_EMCal_Ep->GetYaxis()->SetTitle("Counts");
-        h2_EMCal_Ep->SetMarkerStyle(kOpenCircle);
+        h2_EMCal_Ep->SetFillColor(46); 
+      //  h2_EMCal_Ep->SetMarkerStyle(kOpenCircle);
         // h2_EMCal_Ep->SetMarkerStyle(24);
-        h2_EMCal_Ep->SetMarkerColor(46);
+      //  h2_EMCal_Ep->SetMarkerColor(46);
 
   TH1F *he_eff = new TH1F("he_eff","Electrons efficiency",40,0,4);
         he_eff->SetFillColorAlpha(40, 0.35);
@@ -57,8 +59,8 @@ TFile *file2 = TFile::Open("G4EICDetector_out_pi.root");
 
        // h1_EMCal_Ep->Scale(1./(nbintstotal));
        // h2_EMCal_Ep->Scale(1./(nbintstotal));
-           h1_EMCal_Ep->Scale(1./(h1_EMCal_Ep)->Integral());
-           h2_EMCal_Ep->Scale(1./(h2_EMCal_Ep)->Integral());
+        //   h1_EMCal_Ep->Scale(1./(h1_EMCal_Ep)->Integral());
+        //   h2_EMCal_Ep->Scale(1./(h2_EMCal_Ep)->Integral());
 
   TCanvas *c1 = new TCanvas("c12","EMCal E/p");
   gStyle->SetOptStat(false);
@@ -77,8 +79,11 @@ TFile *file2 = TFile::Open("G4EICDetector_out_pi.root");
   leg->SetFillColor(0);
   leg->SetFillStyle(1001);
   leg->AddEntry("","Particle","");
-  leg->AddEntry("h1_EMCal_Ep","e^{-}","lep");
-  leg->AddEntry("h2_EMCal_Ep","#pi^{-}","lep");
+ // leg->AddEntry("h1_EMCal_Ep","e^{-}","lep");
+ // leg->AddEntry("h2_EMCal_Ep","#pi^{-}","lep");
+  leg->AddEntry("h1_EMCal_Ep","e^{-}","f");
+  leg->AddEntry("h2_EMCal_Ep","#pi^{-}","f");
+
   leg->Draw();
   c1->SaveAs("Ep_pie.pdf");
   
